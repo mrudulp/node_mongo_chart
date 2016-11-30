80bb61510afaa1d42df0ce5775a0ff5281691f1e
Mrudul P <mrudulpen@hotmail.com>
2016-11-30 17:23:42 +0200

    More modular Promisejs code

--- server/js/chartBackend.js
+++ server/js/chartBackend.js
@@ -1,5 +1,4 @@
 var createAllGraphs = function(callback){
-
             // createPlatformDataSets(function(err, callbackObj){
             //     if (err)  throw err
             //     return callback(null,callbackObj)
@@ -8,6 +7,166 @@ var createAllGraphs = function(callback){
             //asyncCreatePlatformDataSets()
             asyncPromissedCreatePlatformDataSets()
 
+            function asyncPromissedCreatePlatformDataSets(){
+                var platformDataSets = []
+
+                loadPlatforms().then(function(platforms){
+                    // console.log(platforms)
+                    var maxPlatCnt = platforms.length
+                    if (maxPlatCnt == 0){
+                        console.log("Bad PlatfQ Query")
+                        callback(true)
+                    }
+                    var platformProcessed = 0
+                    platforms.map(function(platform){
+                        //platformDataSet per platform
+                        // console.log("Before LoadVersion within platform map::"+platform)
+                        var options = createOptions(platform);
+                        loadVersions(platform).then(function(versions){
+                            // console.log("plat::"+platform+":versions:"+versions)
+                            var maxVerCnt = versions.length
+                            if (maxVerCnt == 0){
+                                db.close()
+                                console.log("Bad Versionq Query")
+                                callback(true)
+                            }
+                            var versionDataProcessed = 0
+                            var dataSets = [] // Datasets of all versions for given platform
+                            var labels = [] // For all versions for given platform
+                            versions.map(function(version, vdx){
+                                // Dataset Per Version
+                                // console.log("Before loaddata within version map :: "+version+":plat:"+platform)
+                                loadData(platform, version).then(function(resultSet){
+                                    //call custom method here
+                                    console.log("Data Loaded::Count"+resultSet.length)
+                                    // return resultSet
+
+                                    dataSets.push(getDataSet(resultSet, labels, version, vdx))
+                                    versionDataProcessed++
+
+                                    if (maxVerCnt == versionDataProcessed){
+                                        
+                                        var platformData = {"options":options, "labels":labels, "datasets":dataSets, "platform":platform}
+                                        platformDataSets.push(platformData)
+                                        platformProcessed++ // Processed one version, add platform count
+
+                                        if (platformProcessed == maxPlatCnt){
+                                            // db.close()
+                                            callback(null,platformDataSets)
+                                        }
+                                    }
+                                    //dataset per data, labels per data
+                                })
+                                // console.log("After loaddata within version map::"+version+":plat:"+platform)
+                            })
+                            // console.log("datasets::"+verDS)
+                            // console.log("After Version Map::"+platform)
+                        })
+                        // console.log("After LoadVersion within Platform Map")
+                    })
+                    // console.log(":PlatformDS:"+platformDS)
+                    // console.log("After Platform map")
+                })
+            }// asyncPromissedCreatePlatformDataSets
+
+
+            function loadPlatforms(){
+                var promise = new Promise(function(resolve, reject){
+
+                    var dbHost = "mongodb://mongo:27017/perfSample";
+                    var mongodb = require('mongodb')
+                    var MongoClient = mongodb.MongoClient;
+
+                    //Connecting to the Mongodb instance.
+                    //Make sure your mongodb daemon mongod is running on port 27017 on localhost
+                    MongoClient.connect(dbHost).then(function(db){
+                        var collection = db.collection("perfR")
+                        console.log("Distinct Platform Q going")
+                        var platformq = "platform"
+                        resolve(collection.distinct(platformq))   
+                    })
+                });
+                return promise
+            }
+
+            function loadVersions(platform){
+                var promise = new Promise(function(resolve, reject){
+                    var dbHost = "mongodb://mongo:27017/perfSample";
+                    var mongodb = require('mongodb')
+                    var MongoClient = mongodb.MongoClient;
+
+                    //Connecting to the Mongodb instance.
+                    //Make sure your mongodb daemon mongod is running on port 27017 on localhost
+                    MongoClient.connect(dbHost).then(function(db){
+                        var collection = db.collection("perfR")
+                        console.log("Distinct Version Q going")
+                        var versionq = {"platform": platform }
+                        resolve(collection.distinct("version",versionq))   
+                    })
+                });
+                return promise
+            }
+
+            function loadData(platform, version){
+                var promise = new Promise(function(resolve, reject){
+                    var dbHost = "mongodb://mongo:27017/perfSample";
+                    var mongodb = require('mongodb')
+                    var MongoClient = mongodb.MongoClient;
+
+                    //Connecting to the Mongodb instance.
+                    //Make sure your mongodb daemon mongod is running on port 27017 on localhost
+                    MongoClient.connect(dbHost).then(function(db){
+                        var collection = db.collection("perfR")
+                        console.log("Data Q going")
+                        var dataq = {platform:platform ,version:version}
+                        var varsReq = {filename:1,secs:1,_id:0}
+
+                        //collection.find(dataq,varsReq).toArray().then(function(docs){
+                        resolve(collection.find(dataq,varsReq).toArray())
+                    })
+                });
+                return promise
+            }
+
+            function getDataSet(dataResultSet, labels, version, vdx){
+                var bgColorSet = []
+                var borderColorSet = []
+                var data = []
+                // var version = versions[v]; //Is this accessible??
+                var borderWidth = 1;
+                var type = 'bar';
+                var bgColorArray = [
+                            'rgba(255, 99, 132, 0.2)',
+                            'rgba(54, 162, 235, 0.2)',
+                            'rgba(255, 206, 86, 0.2)',
+                            'rgba(75, 192, 192, 0.2)'
+                            ];
+
+                var borderColorArray = [
+                                'rgba(255, 99, 132, 1)',
+                                'rgba(54, 162, 235, 1)',
+                                'rgba(255, 206, 86, 1)',
+                                'rgba(75, 192, 192, 1)'
+                        ];
+
+                for (i=0; i < dataResultSet.length; i++)
+                    bgColorSet.push(bgColorArray[vdx]);//Per plaform color == idx
+
+
+                for (i=0; i < dataResultSet.length; i++)
+                    borderColorSet.push(borderColorArray[vdx]);//Per plaform color == idx
+
+
+                for (i=0; i < dataResultSet.length; i++)
+                    data.push(dataResultSet[i]["secs"])
+
+                for (i=0; i < dataResultSet.length; i++)
+                    labels.push(dataResultSet[i]["filename"]) // Is this label accessible
+
+                return createDataSet(version, type, data, bgColorSet, borderColorSet, borderWidth ); // Is Dataset accessible
+            }
+
+            /*
             function asyncPromissedCreatePlatformDataSets(){
                 var dbHost = "mongodb://mongo:27017/perfSample";
                 var mongodb = require('mongodb')
@@ -56,29 +215,36 @@ var createAllGraphs = function(callback){
                                                                     'rgba(255, 206, 86, 1)',
                                                                     'rgba(75, 192, 192, 1)'
                                                             ];
-                                                    var result = dataResultSet.map(function(data, idx){
-                                                        console.log("Data::"+data["secs"]+"::"+data["filename"]+"::"+idx)
-                                                        bgColorSet = bgColorArray[vdx]
-                                                        borderColorSet = borderColorArray[vdx]
-                                                        labels.push(data["filename"])
+                                                    var bgColorSet = []
+                                                    var borderColorSet = []
+                                                    var data = []
+                                                    var data = dataResultSet.map(function(dataObj, idx){
+                                                        console.log("Data::"+dataObj["secs"]+"::"+dataObj["filename"]+"::"+idx)
+                                                        bgColorSet.push(bgColorArray[vdx])
+                                                        borderColorSet.push(borderColorArray[vdx])
+                                                        labels.push(dataObj["filename"])
+                                                        return dataObj["secs"]
                                                         //do we need to collect together or individual datasets would work??
-                                                        return createDataSet(version, type, data["secs"], bgColorSet, borderColorSet, borderWidth )
                                                     })
-                                                    console.log("Result.ver::"+result.version+"::R.ty"+result.type+":R.data:"+result.data+":plat:"+platform+":ver:"+version)
-                                                    return result
+                                                    console.log("Creating dataset after data map")
+                                                    Promise.resolve(createDataSet(version, type, data, bgColorSet, borderColorSet, borderWidth ))
                                                 })
+                                                console.log("Returning Version Map")
                                         })
-                                        console.log("PlatRes::"+platRes)
+                                        console.log("PlatRes::"+platRes + ":Labels:"+labels+":options:"+options+":plt:"+platform)
                                         // var platformData = {"options":options, "labels":labels, "datasets":platRes, "platform":platform}
                                         // return platformData
                                 })
-                                
+                                console.log(":Distinct Version")
                         })
                         console.log("::Next Distinct platform::"+platformDataSets)
                     })
+                    console.log("::Done with distinct platform")
                 })
-                return "Returning from asyncPromissedCreatePlatformDataSets"
+                console.log("Returning from asyncPromissedCreatePlatformDataSets")
             }
+            */
+
             function asyncCreatePlatformDataSets() {
     
                 var dbHost = "mongodb://mongo:27017/perfSample";
