74b0c1656d18792720d6997863ae1156e37dc929
Mrudul Pendharkar <mrudul.pendharkar.ext@nokia.com>
2016-12-07 13:29:45 +0200

    Updating python libs

--- python/Todos.md
+++ python/Todos.md
@@ -1,6 +1,9 @@
-a) Argument parser
+a) Argument parser -- Done
+a.1) Use Config parser in dbops -- Done
+a.2) Use Config parser in ozolibs -- Done
+a.3) Use new structure in robot -- Done
 
-b) Use dbops to add data to db
+b) Use dbops to add data to db -- Done
 
 c) Test Multiple entries for a version
 
@@ -11,3 +14,12 @@ e) change route based on version
 f) change route based on filename
 
 g) Pass filename(s), version(s) in callbackObj
+
+h) extract username of system and use it as default db -- Done
+
+i) Extract creator version number through commandline
+
+j) timestamps as another parameter to db
+k) use or show timestamp parameter in graph
+l) show min,max and median in graph 
+m) Extract stitching time from commandline
--- python/configp.py
+++ python/configp.py
@@ -1,20 +1,23 @@
+#python configp.py -f test_config.ini -v -njs -ro -mdb
 import ConfigParser
 import argparse
+import os
 
 class ConfigP(object):
 
     def __init__(self, fname):
         self.Config = ConfigParser.ConfigParser()
-        self.Config.read(fname)
+        self.Config.read(os.path.abspath(fname))
 
-    def ConfigSectionMap(section):
+    def ConfigSectionMap(self, section):
         dict1 = {}
         options = self.Config.options(section)
+        print("Options:{}".format(options))
         for option in options:
             try:
                 dict1[option] = self.Config.get(section, option)
                 if dict1[option] == -1:
-                    DebugPrint("skip: %s" % option)
+                    print("skip: %s" % option)
             except:
                 print("exception on %s!" % option)
                 dict1[option] = None
@@ -23,7 +26,37 @@ class ConfigP(object):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description=" Configuration Parser reads relevant ini given to the program")
-    parser.add_argument('string', metavar='<FILE>', type='string', nargs='+', help='Config file name')
+    parser.add_argument("-f","--configFile", metavar='<CONFIG FILE>', help='Config file name')
+    parser.add_argument("-v","--verbose",  help='Print parsed config data', action='store_true')
+    parser.add_argument("-mdb","--mongo",  help='Parse only mongodb config data', action='store_true')
+    parser.add_argument("-njs","--nodejs",  help='Parse only nodejs config data', action='store_true')
+    parser.add_argument("-ro","--robot",  help='Parse only robot config data', action='store_true')
+
     args = parser.parse_args()
-    config = ConfigP(args)
+    config = ""
+    if args.configFile:
+        config = ConfigP(args.configFile)
+    else:
+        config = ConfigP("test_config.ini")
+    
+    if args.mongo:
+        cmap = config.ConfigSectionMap("MongoDb")
+        if args.verbose:
+            print(cmap["mongo_db"])
+            print(cmap["mongo_collection"])
+            print(cmap["mongo_port"])
+            print(cmap["mongo_host"])
+    
+    if args.nodejs:
+        cmap = config.ConfigSectionMap("NodeJs")
+        if args.verbose:
+            print(cmap["nodejs_port"])
+            print(cmap["nodejs_host"])
+    
+    if args.robot:
+        cmap = config.ConfigSectionMap("Robot")
+        if args.verbose:
+            print(cmap["robot_jsondir"])
+            print(cmap["robot_capturedir"])
+            print(cmap["robot_outputdir"])
 
