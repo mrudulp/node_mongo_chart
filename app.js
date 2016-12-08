217bea7bdf893c281cc939892c45bef0efd8326f
MVP <mrudulpen@hotmail.com>
2016-12-08 21:50:22 +0200

    Changes to handle user based db call

--- app.js
+++ app.js
@@ -2,8 +2,8 @@
 
 // Import Jsons to MongoDB
 // mongoimport -d perfSample -c perfR --type json --file eg_win.json --jsonArray ; mongoimport -d perfSample -c perfR --type json --file eg_mac.json --jsonArray
-// docker cp sample_mac.json nodemongochart_mongo_1:/home ; docker cp sample_win.json nodemongochart_mongo_1:/home ; docker exec -it nodemongochart_mongo_1 /bin/bash
-// docker cp eg_mac.json nodemongochart_mongo_1:/home ; docker cp eg_win.json nodemongochart_mongo_1:/home ; docker exec -it nodemongochart_mongo_1 /bin/bash
+// docker cp sample_mac.json nodemongochart_mongodb_1:/home ; docker cp sample_win.json nodemongochart_mongodb_1:/home ; docker exec -it nodemongochart_mongodb_1 /bin/bash
+// docker cp eg_mac.json nodemongochart_mongodb_1:/home ; docker cp eg_win.json nodemongochart_mongodb_1:/home ; docker exec -it nodemongochart_mongodb_1 /bin/bash
 //MongoDB connection URL - mongodb://host:port/dbName
 // Why mongo --https://docs.docker.com/compose/networking/ (Section for db)
 
@@ -25,14 +25,16 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname+'/index.html'));
 });
 
-app.get("/perfs", function(req, res){
+app.get("/perfs/:user", function(req, res){
 //    res.sendFile(path.join(__dirname+'/index.html'));
-  getData(res);  
+  var user = req.params['user'];
+  console.log("User::"+user)
+  getData(user, res);  
 });
 
 
-function getData(res){
-  chart.createAllGraphs(function(err,callbackObj){
+function getData(user, res){
+  chart.createAllGraphs(user,function(err, callbackObj){
     res.send(callbackObj)
   });
  }
