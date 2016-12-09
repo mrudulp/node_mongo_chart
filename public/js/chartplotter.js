32611c704ec6c02fffe35497a16711a9a12f735c
Mrudul P <mrudulpen@hotmail.com>
2016-12-09 08:04:59 +0200

    Form Submission code

--- public/js/chartplotter.js
+++ public/js/chartplotter.js
@@ -1,7 +1,21 @@
 $(function(){
+    console.log("Document loaded");
+});
+  
+$( "#userForm" ).submit(function( event ) {
+    // Stop form from submitting normally
+    event.preventDefault();
+
+    user = $("input[name='user']").val()
+    url = "http://localhost:8080/perfs/"+user
+    fetchData(url)
+    });
+    
+
+function fetchData(url){
     console.log("sending ajax request to /perfs");
     $.ajax({
-        url: 'http://localhost:8080/perfs',
+        url: url,
         type: 'GET',
         success : function(data) {
                 var platformDataSets = data;
@@ -10,10 +24,14 @@ $(function(){
                 for (p=0; p < platformDataSets.length; p++){
                     createGraph(platformDataSets[p].platform, platformDataSets[p].labels, platformDataSets[p].options, platformDataSets[p].datasets);
                 }
+        },
+        error: function(data){
+            console.log("Error")
         }
-    });
+    });  
+}
 
-    function createGraph(id, aLabels, aOptions, aDataSets){
+function createGraph(id, aLabels, aOptions, aDataSets){
         console.log(id);
         var plot = document.getElementById("plots")
         $(".plots").append("<div style='width:49%; height:50%;float:left'><canvas id="+id+" width='100' height='100' style='border:1px solid;'></canvas></div>");
@@ -28,5 +46,4 @@ $(function(){
             options: aOptions
             });
         return myChart;
-    }
-});
+}
