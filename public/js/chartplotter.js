ca6e8cacd6c36f8270a44d1659dca32428b42152
MVP <mrudulpen@hotmail.com>
2016-12-09 08:04:59 +0200

    Form Submission code

--- public/js/chartplotter.js
+++ public/js/chartplotter.js
@@ -1,7 +1,21 @@
 $(function(){
+    console.log("Document loaded");
+});
+  
+$( "#userForm" ).submit(function( event ) {
+    // Stop form from submitting normally
+    event.preventDefault();
+
+    user = $("input[name='user']").val()
+    url = "http://localhost:8080/perfs/"+user
+    fetchData(url)
+    });
+    
+
+function fetchData(url){
     console.log("sending ajax request to /perfs");
     $.ajax({
-        url: 'http://localhost:8080/perfs',
+        url: url,
         type: 'GET',
         success : function(data) {
                 var platformDataSets = data;
@@ -10,20 +24,23 @@ $(function(){
                 for (p=0; p < platformDataSets.length; p++){
                     createGraph(platformDataSets[p].platform, platformDataSets[p].labels, platformDataSets[p].options, platformDataSets[p].datasets);
                 }
+        },
+        error: function(data){
+            console.log("Error")
         }
-    });
+    });  
+}
 
-    function createGraph(id, aLabels, aOptions, aDataSets){
-        console.log(id);
-        var ctx = document.getElementById(id);
-        var myChart = new Chart(ctx, {
-            type: 'bar',
-            data: {
-                labels: aLabels,
-                datasets: aDataSets
-            },
-            options: aOptions
-            });
-        return myChart;
-    }
-});
+function createGraph(id, aLabels, aOptions, aDataSets){
+    console.log(id);
+    var ctx = document.getElementById(id);
+    var myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: aLabels,
+            datasets: aDataSets
+        },
+        options: aOptions
+        });
+    return myChart;
+}
